b5e9a22a2ab7f02338615afc8140b92757b22872
Brendt McFeeley <brendtmcfeeley@gmail.com>
2017-11-30 13:34:06 -1000

    Displays firebase data and reads in data for taken times

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ let dateTime = [];
 let userData = [];
 
 function writeUserData(resultValue) {
-  console.log(resultValue);
   if (confirmRequiredData() === 2) {
     client.get('https://scheduler-b7ece.firebaseio.com/scheduler.json?shallow=true', function (response) {
       firebase.database().ref('scheduler/' + (Math.floor(response.length / 9) + 1)).set({
@@ -28,80 +27,70 @@ function writeUserData(resultValue) {
         taskLink: resultValue[0][2]
       });
     });
-    alert('Event added to the database!')
+    swal(
+        'Success',
+        'Event added to the database!',
+        'success'
+    )
   } else {
-    alert('Please fill in the required fields');
+    swal(
+        'An error occured',
+        'Please fill out all required fields!',
+        'error'
+    )
   }
 }
 
 function readUserData() {
-  // firebase.database().ref('scheduler/').once('value').then(function (snapshot) {
-  //   for (let i = 1; i <= snapshot.val().length - 1; i++) {
-  //     // let name = document.createTextNode(snapshot.val()[i].name);
-  //     // let location = document.createTextNode(snapshot.val()[i].location);
-  //     // let date = document.createTextNode(snapshot.val()[i].date);
-  //     // let social = document.createTextNode(snapshot.val()[i].social);
-  //     // let website = document.createTextNode(snapshot.val()[i].website);
-  //     // let desc = document.createTextNode(snapshot.val()[i].desc);
-  //     // let timeStart = document.createTextNode(snapshot.val()[i].timeStart);
-  //     // let timeEnd = document.createTextNode(snapshot.val()[i].timeEnd);
-  //     createDiv(name, location, date, social, website, desc, timeStart, timeEnd);
-  //     appendItems(name, location, date, social, website, desc, timeStart, timeEnd);
-  //     storeData(name, location, date, social, website, desc, timeStart, timeEnd);
-  //   }
-  // })
-  let taskName = 'Club Meeting';
-  let taskDate = '12/01/2017';
-  let taskStart = '1:00 PM';
-  let taskEnd = '3:00 PM';
-  let desc = 'Club meeting today at 1!';
-  let link = 'www.neopets.com';
+  let taskNo = 1;
+  firebase.database().ref('scheduler/').once('value').then(function (snapshot) {
+    for (let i = 1; i <= snapshot.val().length - 1; i++) {
+      let taskDateFB = document.createTextNode(snapshot.val()[i].date);
+      let taskDesc = document.createTextNode(snapshot.val()[i].taskDesc);
+      let taskLink = document.createTextNode(snapshot.val()[i].taskLink);
+      let taskName = document.createTextNode(snapshot.val()[i].taskName);
+      let timeEndFB = document.createTextNode(snapshot.val()[i].timeEnd);
+      let timeStartFB = document.createTextNode(snapshot.val()[i].timeStart);
 
+      createAndAppendDiv(taskNo, taskDateFB, taskDesc, taskLink, taskName, timeEndFB, timeStartFB);
+      ++taskNo;
+    }
+  })
+}
+
+function getDateTime() {
+  firebase.database().ref('scheduler/').once('value').then(function (snapshot) {
+    for (let i = 1; i <= snapshot.val().length - 1; i++) {
+      dateTime.push(snapshot.val()[i].date);
+      dateTime.push(parseInt(snapshot.val()[i].timeStart.split(':')[0]));
+    }
+  })
+}
+
+function createAndAppendDiv(taskNo, taskDisplayDate, taskDesc, taskLink, taskName, taskDisplayEnd, taskDisplayStart) {
   let div = document.getElementById('showData');
-  div.append('Task Name : ' + taskName);
   div.appendChild(document.createElement('br'));
-  div.append('Task Date : ' + taskDate);
+  div.append('Task Number : ' + taskNo);
   div.appendChild(document.createElement('br'));
-  div.append('Task Start Time : ' + taskStart);
+  div.append('Task Name : ');
+  div.appendChild(taskName);
   div.appendChild(document.createElement('br'));
-  div.append('Task End Time : ' + taskEnd);
+  div.append('Task Date : ');
+  div.appendChild(taskDisplayDate);
   div.appendChild(document.createElement('br'));
-  div.append('Task Description : ' + desc);
+  div.append('Task Start : ');
+  div.appendChild(taskDisplayStart);
   div.appendChild(document.createElement('br'));
-  div.append('Task Link : ' + link);
-  // let eventDiv = '<div class="ui grid"><div class="four wide column">' + taskName + '</div>' + '<div class="four wide column"><div id="eventName">' + taskDate + '</div>' + '<div id="location">' + taskStart + '</div>' + '<div id="addressTitle">' taskEnd + '</div>' + '<div id="phoneTitle">Phone: ' + myData[0][index].number + '</div></div>' + '</div>';
-
-}
-
-function createDiv(name, location, date, social, website, desc, timeStart, timeEnd) {
-  let eventDiv = '<div class="ui grid"><div class="four wide column">' + date + '</div>' + '<div class="eight wide column"><div id="eventName">' + name + '</div>' + '<div id="location">' + location + '</div>' + '<div id="addressTitle">Address: ' + myData[0][index].address + '</div>' + '<div id="phoneTitle">Phone: ' + myData[0][index].number + '</div></div>' + '</div>';
-}
-
-function storeData(name, location, date, social, website, desc, timeStart, timeEnd) {
-  nameEvents.push(name);
-}
-
-function appendItems(name, location, date, social, website, desc, timeStart, timeEnd) {
-  let div = document.getElementById('showData');
-  div.appendChild(name);
+  div.append('Task End : ');
+  div.appendChild(taskDisplayEnd);
   div.appendChild(document.createElement('br'));
-  div.appendChild(location);
+  div.append('Task Description : ');
+  div.appendChild(taskDesc);
   div.appendChild(document.createElement('br'));
-  div.appendChild(date);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(social);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(website);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(desc);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(timeStart);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(timeEnd);
+  div.append('Task Link : ');
+  div.appendChild(taskLink);
   div.appendChild(document.createElement('br'));
 }
-
-
 
 function confirmRequiredData() {
   let req = document.getElementsByClassName('requiredInput');
@@ -123,7 +112,6 @@ function confirmDateTime() {
       ++retVal;
     }
   }
-  console.log(retVal);
   return retVal - 1;
 }
 
@@ -201,10 +189,6 @@ function collectUserData() {
       swal({
         title: 'All done!',
         type: 'success',
-        // html:
-        // 'Your answers: <pre>' +
-        // JSON.stringify(result.value) +
-        // '</pre>',
         confirmButtonText: 'Garenz Bo Barenz!'
       }).then((result) => {
         writeUserData(userData);
@@ -213,12 +197,6 @@ function collectUserData() {
     }
   })
 }
-
-// function test() {
-//   console.log(userData);
-//   console.log(userData[0]);
-//   console.log(userData[0][0]);
-// }
 
 function processData() {
   if (confirmRequiredData() === 2) {
